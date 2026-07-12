44bfbad92dae9ab5e99ba7a59f36a8b9f6ffe175
rubayatrifat <rubayatislam17@gmail.com>
2026-07-12 11:49:42 +0600

    this keyword basic

---
+++ this Keyword/main.js
@@ -0,0 +1,38 @@
+// This in global scope
+console.log(this)
+
+// This in function
+function thisKey() {
+    console.log(this)
+}
+
+// thisKey()
+
+// this in method
+const obj = {
+    name: "Pablo",
+    func: function() {
+        console.log(this.name)
+    }
+}
+
+obj.func()
+
+
+// This in event handler
+document.querySelector("h1").addEventListener("click", () => {
+    console.log(this) // Output: the H1
+})
+
+
+// This behaviors
+/**
+ * This in global  - window
+ * this inside a func - window
+ * method with es5 func - obj
+ * method with es6 arrow func - window
+ * method with es5 func inside a es5 func - window
+ * method with es6 func inside a es5 func - obj
+ * event handler - element
+ * class - blank obj
+ */
