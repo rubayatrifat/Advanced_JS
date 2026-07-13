61ca194fd8e4ef287305c9610a5a30e64241bd77
rubayatrifat <rubayatislam17@gmail.com>
2026-07-13 13:38:02 +0600

    OOP (JavaScript)

---
+++ OOP (JavaScript)/main.js
@@ -0,0 +1,30 @@
+// // Constructor Function
+// function UserData(name, role, age) {
+//     this.name = name,
+//     this.role = role,
+//     this.age = age,
+//     this.bio = `Hi there!! I'm ${name}, I'm a ${role} and I'm ${age} years old`
+// }
+
+// UserData.prototype.company = "C.Devs"
+
+// let user1Bio = new UserData("Manush", "Front-end Developer", 22)
+// let user2Bio = new UserData("Omuk", "Back-end Developer", 26)
+// let user3Bio = new UserData("Rifat", "System Designer", 28)
+
+
+
+// console.log(user1Bio.bio)
+// console.log(user2Bio.bio)
+// console.log(user3Bio.bio)
+
+// console.log(user1Bio.company)
+// console.log(user2Bio.company)
+// console.log(user3Bio.company)
+
+
+
+// ===== CLASSes
+
+
+
