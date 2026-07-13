5f0f66f32250f742815e5c67b5dfe191eeaf613c
rubayatrifat <rubayatislam17@gmail.com>
2026-07-13 14:08:43 +0600

    JavaScript Classes

--- OOP (JavaScript)/main.js
+++ OOP (JavaScript)/main.js
@@ -26,5 +26,45 @@
 
 // ===== CLASSes
 
+class CreateUser {
+    constructor(name, email, color) {
+        this.name = name
+        this.email = email
+        this.color = color
+        this.role = "user"
+    }
 
+    write(text) {
+        const h1 = document.createElement("h1")
+        h1.textContent = `${this.name} (${this.role}): ${text}`
+        h1.style.color = this.color
+        document.body.appendChild(h1)
+    }
+}
 
+class CreateAdmin extends CreateUser {
+    constructor(name, email, color) {
+        super(name, email, color)
+        this.role = "admin"
+    }
+
+    eraseMsg(userName) {
+        document.querySelectorAll("h1").forEach(item => {
+            if(item.style.color === userName.color) {
+                item.remove()
+            }
+        })
+    }
+}
+
+const user1 = new CreateUser("Tobali Tamuk", "tamik@gfma.com", "red")
+const user2 = new CreateUser("Estofa Sarder", "namki@asdf.com", "blue")
+const admin1 = new CreateAdmin("Mosa Sarwar", "musasorwar@tuf.com", "green")
+
+user1.write("Hlw")
+user2.write("Fuck u")
+user1.write("Brother")
+user2.write("Fuck uuuuuuuuuuuu")
+admin1.write("Mind ur language")
+admin1.write("Imma delete ur msg")
+admin1.eraseMsg(user2)
